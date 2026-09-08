e473aa99d73157b3e510dcc8d470e2cd7b394766
Alicja Gorgoń <fejkowy@email.com>
2026-09-08 09:43:22 +0200

    pierwsze cwiczenia Java

--- ProjektTestowy1/src/Main.java
+++ ProjektTestowy1/src/Main.java
@@ -1,13 +1,39 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    System.out.println("Hello World"); // sout
+    System.out.println("Cokolwiek");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    // To jest komentarz jednoliniowy (ctrl + /)
+
+    /*
+    To jest komentarz
+    Na wiele linijek
+    Coś tu opisuję bardziej
+     */
+
+    int calkowita = 5;
+    double rzeczywista = 1.5;
+
+    System.out.println("\tWartość zmiennej \"calkowita\" to " + calkowita/2);
+    System.out.println("\nWartość zmiennej rzeczywista to " + rzeczywista);
+
+    char litera = 97;
+    String napis = "napis";
+
+    System.out.println(litera);
+    System.out.println(napis);
+
+    boolean czyPrawda = true; // false
+
+    System.out.println(czyPrawda);
+
+    System.out.println("Wartość 1 + 2 + 3 = " + (1 + 2 + 3));
+    System.out.println(1 + 2 + 3 + " to wartość 1 + 2 + 3");
+
+    System.out.println("Podaj wiek: ");
+
+    Scanner sc = new Scanner(System.in);
+
+    int wiek = sc.nextInt();
 }
